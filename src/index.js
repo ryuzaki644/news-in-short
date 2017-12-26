cf0cfbdc83ec2abeb5a564141aba7c814a048c24
ryuzaki644 <sathwikkoushal@gmail.com>
2017-12-26 23:23:06 +0530

    Modify createStore to persist data and subscribe for store changes
      - In createStore add persistedState to save state to localStorage
      - Use store's subscribe function to update the localStorage as the
    store changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {loadState, saveState} from './localStorage'
 import App from './App'
 // import registerServiceWorker from './registerServiceWorker'
 import './index.css'
@@ -21,8 +22,12 @@ const AppWithRouter = () => {
   )
 }
 
-let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const persistedState = loadState()
+const store = createStore(reducers, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+store.subscribe(() => {
+  saveState(store.getState())
+})
 ReactDOM.render(
   <Provider store={store}>
     <AppWithRouter />
