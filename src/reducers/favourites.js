52e259dc11865ac4a8debe9c158e3b5693e94148
ryuzaki644 <sathwikkoushal@gmail.com>
2017-12-26 11:23:59 +0530

    Add toggling functionality to sources in favourites
     - On click of favourites, if source already exists, remove from the
    state and viceversa

--- src/reducers/favourites.js
+++ src/reducers/favourites.js
@@ -3,27 +3,37 @@ const initialState = {
   allCategories: []
 }
 
-const concatUniqSources = (existingSources, newSource) => {
-  // existting sources will be array of sources for that category
-  // new source is a string
-  const filterSource = existingSources.filter(source => source === newSource)
-  const doesSourceExist = filterSource.length !== 0
-  return doesSourceExist ? existingSources : existingSources.concat(newSource)
+// Function for toggling the source
+const updateSource = (stateFavourites, actionCategory, actionSource, indexOfSource) => {
+  if (indexOfSource === -1) {
+    return Object.assign({}, stateFavourites, {
+      [actionCategory]: stateFavourites[actionCategory].concat(actionSource)
+    })
+  }
+
+  const sourceInState = stateFavourites[actionCategory]
+  const leftArray = sourceInState.slice(0, indexOfSource)
+  const rightArray = sourceInState.slice(indexOfSource + 1)
+  return Object.assign({}, stateFavourites, {
+    [actionCategory]: leftArray.concat(rightArray)
+  })
 }
 
 export const favouriteSourcesWithCategory = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_FAVOURITES':
-      if (state.favourites[action.category] === undefined) {
-        const newFavCategory = Object.assign({}, state.favourites, {[action.category]: [action.source]})
-        const allCategories = state.allCategories.concat(action.category)
-        return {...state, favourites: newFavCategory, allCategories}
+      switch (state.favourites[action.category]) {
+        case undefined:
+          const newFavCategory = Object.assign({}, state.favourites, {[action.category]: [action.source]})
+          const allCategories = state.allCategories.concat(action.category)
+          return {...state, favourites: newFavCategory, allCategories}
+        default:
+          const indexOfSource = state.favourites[action.category].indexOf(action.source)
+          return {
+            ...state,
+            favourites: updateSource(state.favourites, action.category, action.source, indexOfSource)
+          }
       }
-      const updatedFavCategory = Object.assign(
-          {},
-          state.favourites,
-          {[action.category]: concatUniqSources(state.favourites[action.category], action.source)})
-      return {...state, favourites: updatedFavCategory}
     default:
       return state
   }
